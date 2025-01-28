998921c3ba6394b700684e58e0bd18fb1f4d0d17
NakonechniyVitaliy <vetalpro30@gmail.com>
2025-01-28 23:23:05 +0200

    some-changes

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,25 +4,21 @@ namespace App\Controller\Admin;
 
 namespace App\Controller\Admin;
 
-use App\Entity\Country;
+use App\Entity\ElectronicCategory;
 use App\Entity\Manufacturer;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin')]
+
 final class DashboardController extends AbstractDashboardController
 {
-    #[Route('/dashboard', methods: ['GET'])]
+    #[Route('/admin', name: 'admin_dashboard', methods: ['GET'])]
     public function index(): Response
     {
-        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
-        $url = $routeBuilder->setController(ManufacturerCrudController::class)->generateUrl();
-
-        return $this->redirect($url);
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -34,8 +30,8 @@ final class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'homepage');
-        yield MenuItem::linkToCrud('Manufacture', 'fas fa-map-marker-alt', Manufacturer::class);
-//        yield MenuItem::linkToCrud('Country', 'fas fa-comments', Country::class);
+        yield MenuItem::linktoRoute('Back to the website', 'fa-solid fa-globe', 'app_main');
+        yield MenuItem::linkToCrud('Manufacture', 'fa-solid fa-hammer', Manufacturer::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-sitemap', ElectronicCategory::class);
     }
 }
